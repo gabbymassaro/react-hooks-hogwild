132ab7d4c3b3c8e38aa41245d10eaeb7129c324a
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-14 14:13:26 -0400

    Finish sort by name and weight

--- src/components/App.js
+++ src/components/App.js
@@ -16,14 +16,34 @@ function App() {
     }
   }
 
+  function handleSortingNames() {
+    if (hogData === hogs) {
+      const sortedNames = hogData.toSorted((a, b) => a.name.localeCompare(b.name))
+      setHogData(sortedNames)
+    } else {
+      setHogData(hogs)
+    }
+  }
+
+  function handleSortingWeight() {
+    if (hogData === hogs) {
+      const sortedWeights = hogData.toSorted((a, b) => a.weight - b.weight)
+      setHogData(sortedWeights)
+    } else {
+      setHogData(hogs)
+    }
+  }
+
 	return (
 		<div className="App">
 			<Nav />
       <div className="smallHeader">{hogData === hogs ? 'All Pigs' : 'Greasy Pigs'} </div>
       <br></br>
       <button className="normalText" onClick={handleFilterChange}>
-          {hogData === hogs ? 'Greasy' : 'All'}
-        </button>
+          {hogData === hogs ? 'Greasy?' : 'All Pigs'}
+      </button>
+      <button className="normalText" onClick={handleSortingNames}>Sort By Name</button>
+      <button className="normalText" onClick={handleSortingWeight}>Sort By Weight</button>
       <PigTiles
         hogData={hogData}
         setHogData={setHogData}
